dbd04e11ad0187396da58812a91e9928eb70c125
NuminousCode <sanchezwebdev@gmail.com>
2024-01-26 12:06:23 -0600

    readme update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+      <Head>
+        <link rel="icon" href="/news-publishing-svgrepo-com.svg" type="image/png"/>       
+        <title>Pasadena Tribune</title>
+      </Head>
       <Component {...pageProps} />
+    </>
   )
 }
 
